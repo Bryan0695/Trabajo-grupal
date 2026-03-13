76243f62e42c7d35f2ad1cd014ee217e562fe566
Bryan0695 <bdf.gallegos@yavirac.edu.ec>
2026-03-13 14:47:58 -0500

    se elimino aarchivos qeu no eran

--- frontend_src_backup/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://TU_DOMINIO_BACKEND.com',         // Reemplazar
-  appUrl: 'https://TU_DOMINIO_FRONTEND.com',         // Reemplazar
-  pluxEstablishmentId: 'TU_ID_ESTABLECIMIENTO_B64',  // Reemplazar
-};
